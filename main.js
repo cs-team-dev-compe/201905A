25b00c414e09acbc96db52ae9747e305c3a65cb0
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2019-05-11 19:41:44 +0900

    fixed link path

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function () {
-    $("#header").load("templates/_header.html");
-    $("#footer").load("templates/_footer.html");
+    $("#header").load("templates/header.html");
+    $("#footer").load("templates/footer.html");
     $("#main").load("/home.html");
 });
 
